54ee0b7111f4918f03b2ea91d18a61318a100114
Seth Jackson <sethjackson@gmail.com>
2025-04-18 14:08:08 -0400

    Denote exception as ignored

--- src/main/java/mazesolver/Program.java
+++ src/main/java/mazesolver/Program.java
@@ -7,7 +7,7 @@ public class Program {
     public static void main(String[] args) {
         try {
             UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
-        } catch (Exception e) {
+        } catch (Exception ignored) {
         }
 
         SwingUtilities.invokeLater(MainWindow::new);

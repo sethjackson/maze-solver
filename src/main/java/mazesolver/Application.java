58c064f3b3c538234b126c617aa5482722931983
Seth Jackson <sethjackson@gmail.com>
2025-05-31 21:15:17 -0400

    Rename the main class

--- src/main/java/mazesolver/Application.java
+++ src/main/java/mazesolver/Application.java
@@ -3,7 +3,7 @@ package mazesolver;
 import javax.swing.SwingUtilities;
 import javax.swing.UIManager;
 
-public class Program {
+public class Application {
     public static void main(String[] args) {
         try {
             UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
